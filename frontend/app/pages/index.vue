6795422f5afb4ece9afcf52491e3e5e7dbfb05a6
tensho1026 <tensho1026@icloud.com>
2025-10-30 12:01:27 +0900

    tailwind

--- frontend/app/pages/index.vue
+++ frontend/app/pages/index.vue
@@ -1,3 +1,3 @@
 <template>
-  <h1>snohwg</h1>
+  <h1 class="text-red-900">snohwg</h1>
 </template>
--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,5 +1,12 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+
+
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
-  devtools: { enabled: true }
-})
+  compatibilityDate: "2025-10-30",
+  css: ["@/assets/css/tailwind.css"],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
+});
