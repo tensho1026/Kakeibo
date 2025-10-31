726b1638c22f07bc85b0ea42ad611d454430b13e
tensho1026 <tensho1026@icloud.com>
2025-10-31 17:17:19 +0900

    log

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -4,6 +4,7 @@ import { Controller, Get } from '@nestjs/common';
 export class AppController {
   @Get()
   getRoot(): string {
+    console.log('NestJS + Docker is working!');
     return 'NestJS + Docker is working!';
   }
 }
