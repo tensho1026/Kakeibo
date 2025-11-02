761d4f1b8331c1be8fb9f62b38a03ad2e461dde8
tensho1026 <tensho1026@icloud.com>
2025-11-02 20:21:30 +0900

    一旦hozonnapi

--- backend/src/modules/transaction/transaction.controller.ts
+++ backend/src/modules/transaction/transaction.controller.ts
@@ -1,4 +1,23 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 
+import { TransactionService } from './transaction.service';
+import { PaymentType, TransactionType } from 'src/entities/transaction.entity';
+
+type Type = {
+  type: TransactionType;
+  date: Date;
+  amount: number;
+  categoryId: number;
+  paymentMethod?: PaymentType;
+  memo?: string;
+  userId: number;
+};
 @Controller('transaction')
-export class TransactionController {}
+export class TransactionController {
+  constructor(private readonly transactionService: TransactionService) {}
+  @Post()
+  create(@Body() data: Type) {
+    console.log('das');
+    return this.transactionService.saveTransaction(data);
+  }
+}
--- backend/src/modules/transaction/transaction.service.ts
+++ backend/src/modules/transaction/transaction.service.ts
@@ -1,4 +1,39 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { DeepPartial, Repository } from 'typeorm';
+
+import { Transaction } from 'src/entities/transaction.entity';
+import { Category } from 'src/entities/category.entity';
+import { User } from 'src/entities/user.entity';
 
 @Injectable()
-export class TransactionService {}
+export class TransactionService {
+  constructor(
+    @InjectRepository(Transaction)
+    private readonly transactionRepository: Repository<Transaction>, // ← Repositoryを注入
+
+    @InjectRepository(Category)
+    private readonly categoryRepository: Repository<Category>, // ← Repositoryを注入
+  ) {}
+
+  saveTransaction(data: {
+    userId: number;
+    type: 'income' | 'expense';
+    date: Date;
+    amount: number;
+    categoryId: number;
+    paymentMethod?: string;
+    memo?: string;
+  }) {
+    const saveData = this.transactionRepository.create({
+      type: data.type,
+      date: data.date,
+      amount: data.amount,
+      paymentMethod: data.paymentMethod,
+      memo: data.memo,
+      category: { id: data.categoryId } as Category,
+      user: { id: data.userId } as User,
+    } as DeepPartial<Transaction>);
+    return this.transactionRepository.save(saveData);
+  }
+}
